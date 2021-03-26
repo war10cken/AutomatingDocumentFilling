7fdba14867881df819f79c7a3d0f00434d5dc1ae
Алексей Тимофеев <garhafar@gmail.com>
2021-03-26 17:49:58 +0300

    Добавлены недостающие файлы

---
+++ AutomatingDocumentFilling.WPF/App.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using AutomatingDocumentFilling.WPF.State.Navigators;
+using AutomatingDocumentFilling.WPF.ViewModels;
+using AutomatingDocumentFilling.WPF.ViewModels.Factories;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AutomatingDocumentFilling.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private IHost _host;
+
+        public App()
+        {
+            _host = CreateHostBuilder().Build();
+        }
+        
+        public static IHostBuilder CreateHostBuilder(string[] args = null)
+        {
+            return Host.CreateDefaultBuilder(args)
+                       .ConfigureServices(services =>
+                        {
+                            services.AddSingleton<IAutomatingDocumentFillingViewModelFactory, AutomatingDocumentFillingViewModelFactory>();
+
+                            services.AddSingleton<CreateViewModel<FirstPageViewModel>>(service => () =>
+                                                                                           new FirstPageViewModel());
+                            
+                            services.AddSingleton<INavigator, Navigator>();
+                            services.AddSingleton<MainViewModel>();
+
+                            services.AddScoped(service => new MainWindow(service.GetRequiredService<MainViewModel>()));
+                        });
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            _host.Start();
+
+            Window window = _host.Services.GetRequiredService<MainWindow>();
+            window.Show();
+            
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            await _host.StopAsync();
+            _host.Dispose();
+            
+            base.OnExit(e);
+        }
+    }
+}
---
+++ AutomatingDocumentFilling.WPF/Views/FirstPageView.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Win32;
+using System.IO;
+using System.Text.RegularExpressions;
+using Xceed.Words.NET;
+
+namespace AutomatingDocumentFilling.WPF.Views
+{
+    public partial class FirstPageView : UserControl
+    {
+        public FirstPageView()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+
+            dlg.DefaultExt = ".docx";
+
+            dlg.Filter = "Word documents (.docx)|*.docx";
+
+            bool? result = dlg.ShowDialog();
+
+
+            if (result == true)
+            {
+                if (dlg.FileName.Length > 0)
+                {
+                    string newDocumentName = string.Concat(Path.GetDirectoryName(dlg.FileName), "\\",
+                                                              Path.GetFileNameWithoutExtension(dlg.FileName), ".docx");
+
+                    using (var document = DocX.Load(newDocumentName))
+                    {
+                        if (document.FindUniqueByPattern(@"<[\w \=]{4,}>", RegexOptions.IgnoreCase).Count > 0)
+                        {
+                            document.ReplaceText("<code>", TextBoxName.Text, false, RegexOptions.IgnoreCase);
+                            document.Save();
+                        }
+                    }
+                }          
+            }
+        }
+    }
+}
